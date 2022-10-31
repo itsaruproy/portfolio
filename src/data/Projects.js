bea42b84c83c5226a2b873368dd2f546e11aa959
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-31 16:39:55 +0530

    feat: Add projects data

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -5,8 +5,9 @@
 export const Projects = [
     {
         name: 'Bloggler',
-        description: 'This is a blogging site',
-        yt: '#',
+        description:
+            'This is a blogging site built using ReactJS, NodeJS, MongoDB, Chakra UI. Users can create an account and share their stories with the world using this web app, they can also find articles, follow their friends and much more',
+        yt: 'https://youtu.be/XAwXbXeFYks',
         github: 'https://github.com/itsaruproy/bloggler-frontend',
     },
     {
@@ -14,13 +15,14 @@ export const Projects = [
         description:
             'This is a Google Drive utility tool that helps users to share their Drive folders more securely',
         yt: '#',
-        github: '#',
+        github: 'https://github.com/itsaruproy/driveshare-frontend',
     },
     {
         name: 'FunChat',
-        description: 'This is a WebSocket based group chat web application',
-        yt: '#',
-        github: '#',
+        description:
+            'This is a WebSocket based group chat web application where users can communicate with a group of people at the same time and same place',
+        yt: 'https://youtu.be/1tMtAA0wMqQ',
+        github: 'https://github.com/itsaruproy/FunChat',
         weblink: 'https://chat-roy.herokuapp.com/',
     },
 ]
